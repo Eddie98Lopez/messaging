e63a89b66b81588b5270801a2d56d7763e8b3f72
Eduardo Lopez <ed98lopez@gmail.com>
2021-10-30 16:15:33 -0700

    The message details component can now access individual messages details through params and display them on the page.The page also has a delete, go back, and reply button. All thats left to do is styling

--- src/components/MessageDetails.js
+++ src/components/MessageDetails.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams, useHistory }from 'react-router-dom'
+import {Icon} from './styled-components'
+import { MessagesContext } from '../utils/MessageListContext'
+import { deleteIcon,reply } from '../design-assets'
+
 
 const MessageDetails = (props) => {
+const{folder, id} = useParams()
+const {push,goBack} = useHistory()
+
+console.log(useHistory())
+
+const [messages,setMessage] = useContext(MessagesContext)
+const details = messages[`${folder}`].filter(item => item.id == id)[0]
+const {title, body, sender, receiver} = details
+
+const deleteMessage = () => {
+    console.log(messages)
+    setMessage({
+        ...messages,
+        [folder]:messages[`${folder}`].filter(item=> item !== details)})
+
+    push(`/dash/${folder}`)
+}
+
+//const [details,setDetails] = useState({})
+
+
     return <div>
-        details
+
+        <div>
+            <Icon alt='go back' onClick={goBack}/>
+            <Icon img={reply} alt = 'reply' onClick={()=>push('/dash/compose')}/>
+        </div>
+
+        <div>
+        <h2>{title}</h2>
+        <h4>{folder === 'sent' ? `to: ${receiver}` : `from: ${sender}`}</h4>
+        <p>{body}</p>
+        </div>
+
+        <div onClick={deleteMessage}>
+            <Icon 
+             img={deleteIcon}
+             alt='delete'/>
+        </div>
+
     </div>
 }
 
