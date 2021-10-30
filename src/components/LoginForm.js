a3c54d2921aa9975875e08f91e1724645ea70aae
Eduardo Lopez <ed98lopez@gmail.com>
2021-10-30 13:10:22 -0700

    added validation and disabled feaure to send button that will become clickable after all fields are required fields are filled.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,80 +1,79 @@
-import React, { useState, useEffect } from 'react'
-import loginSchema from '../utils/loginSchema'
-import * as Yup from 'yup'
+import React, { useState, useEffect } from "react";
+import loginSchema from "../utils/loginSchema";
+import * as Yup from "yup";
 
 const Login = (props) => {
+  const initialForm = {
+    username: "",
+    password: "",
+  };
 
-    const initialForm = {
-        username:'',
-        password:''
-    }
+  const initialErrs = {
+    username: "",
+    password: "",
+  };
 
-    const initialErrs = {
-        username:'',
-        password:''
-    }
-    
-    const [form,setForm] = useState(initialForm)
-    const [errs,setErrs] = useState(initialErrs)
-    const [disabled, setDisabled] = useState(true)
+  const [form, setForm] = useState(initialForm);
+  const [errs, setErrs] = useState(initialErrs);
+  const [disabled, setDisabled] = useState(true);
 
+  const change = (e) => {
+    const { name, value } = e.target;
+    Yup.reach(loginSchema, name)
+      .validate(value)
+      .then((res) => {
+        setErrs({ ...errs, [name]: "" });
+      })
+      .catch((err) => {
+        setErrs({ ...errs, [name]: err.errors[0] });
+      });
+    setForm({ ...form, [name]: value });
+  };
 
-    const change = e => {
-        const {name, value} = e.target
-        Yup.reach(loginSchema,name)
-            .validate(value)
-            .then(res => {
-                setErrs({...errs, [name]:''})
-            })
-            .catch(err => {
-                setErrs({...errs, [name]: err.errors[0]})
-            })
-        setForm({...form, [name]:value})
-    }
-
-    const submit = e => {
-        e.preventDefault()
-        /*axios.post('url',form)
+  const submit = (e) => {
+    e.preventDefault();
+    /*axios.post('url',form)
             .then(res => {
                 console.log(res)
             })
             .catch(err => {
                 console.log(err)
             }) */
-    }
+  };
 
-    useEffect(()=>{
-        loginSchema.isValid(form)
-            .then(res => setDisabled(!res))
-    }, [form])
+  useEffect(() => {
+    loginSchema.isValid(form).then((res) => setDisabled(!res));
+  }, [form]);
 
-    return <div>
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          <input
+            name="username"
+            type="text"
+            value={form.username}
+            onChange={change}
+            placeholder="Username"
+          />
+          <div>{errs.username}</div>
+        </div>
 
-        <form onSubmit={submit}>
-            <div>
-                <input
-                    name='username'
-                    type='text'
-                    value={form.username}
-                    onChange={change}
-                    placeholder='Username'/>
-                <div>{errs.username}</div>
-            </div>
+        <div>
+          <input
+            name="password"
+            type="password"
+            value={form.password}
+            onChange={change}
+            placeholder="Password"
+          />
+          <div>{errs.password}</div>
+        </div>
 
-            <div>
-                <input
-                    name='password'
-                    type='password'
-                    value={form.password}
-                    onChange={change}
-                    placeholder='Password'/>
-                <div>{errs.password}</div>
-            </div>
-
-            <button disabled={disabled}>login</button>
-        </form>
-
+        <button disabled={disabled}>login</button>
+      </form>
     </div>
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/NewMessage.js
+++ src/components/NewMessage.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import messageSchema from "../utils/messageSchema";
 import * as Yup from "yup";
+//import axios from 'axios'
 
 const NewMessage = (props) => {
   const initialMessage = {
@@ -13,19 +15,33 @@ const NewMessage = (props) => {
     receiver: "",
     body: "",
   };
+
   const [message, setMessage] = useState(initialMessage);
   const [errors, setErrors] = useState(initialErrors);
+  const [disabled,setDisabled] = useState(true)
 
   const change = (e) => {
     const { name, value } = e.target;
     setMessage({ ...message, [name]: value });
-    console.log(message);
+    Yup.reach(messageSchema, name)
+      .validate(value)
+      .then((res) => setErrors({ ...errors, [name]: "" }))
+      .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }));
+
   };
 
   const submit = (e) => {
     e.preventDefault();
     setMessage(initialMessage);
+    /*axios.post(url,message)
+        .then()
+        .catch()*/
   };
+
+  useEffect(()=>{
+      messageSchema.isValid(message)
+        .then(res => setDisabled(!res))
+  },[message])
 
   return (
     <div>
@@ -63,7 +79,7 @@ const NewMessage = (props) => {
           <div>{errors.body}</div>
         </div>
 
-        <button>Send</button>
+        <button disabled={disabled}>Send</button>
       </form>
     </div>
   );
