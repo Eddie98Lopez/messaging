d33301e71ff72df120fc6ee368b614bfcc84187e
Eduardo Lopez <ed98lopez@gmail.com>
2021-10-30 12:40:39 -0700

    added the form fields and tested state updates correctly. Next add validation

--- src/components/NewMessage.js
+++ src/components/NewMessage.js
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const NewMessage = (props) => {
+  const initialMessage = {
+    title: "",
+    receiver: "",
+    body: "",
+  };
 
-    return (
-        <div>
-            NewMessage
-        </div>
-    )
-}
+  const [message, setMessage] = useState(initialMessage);
 
-export default NewMessage
+  const change = (e) => {
+    const { name, value } = e.target;
+    setMessage({ ...message, [name]: value });
+    console.log(message);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    setMessage(initialMessage)
+  };
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <input
+          name="title"
+          type="text"
+          value={message.title}
+          onChange={change}
+          placeholder="Title"
+        />
+
+        <input
+          name="receiver"
+          type="text"
+          value={message.receiver}
+          onChange={change}
+          placeholder="Recipient"
+        />
+
+        <textarea
+          type="text"
+          name="body"
+          value={message.body}
+          onChange={change}
+          placeholder="type your message here..."
+        />
+
+        <button>Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewMessage;
