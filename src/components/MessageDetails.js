8b29743dd542f849d93dc94be175ab01c5757190
Eduardo Lopez <ed98lopez@gmail.com>
2021-10-30 18:38:47 -0700

    formatted code

--- src/components/MessageDetails.js
+++ src/components/MessageDetails.js
@@ -1,50 +1,45 @@
-import React, { useContext } from 'react'
-import { useParams, useHistory }from 'react-router-dom'
-import {Icon} from './styled-components'
-import { MessagesContext } from '../utils/MessageListContext'
-import { deleteIcon,reply } from '../design-assets'
-
+import React, { useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { Icon } from "./styled-components";
+import { MessagesContext } from "../utils/MessageListContext";
+import { deleteIcon, reply } from "../design-assets";
 
 const MessageDetails = (props) => {
-const{folder, id} = useParams()
-const {push,goBack} = useHistory()
+  const { folder, id } = useParams();
+  const { push, goBack } = useHistory();
 
-const [messages,setMessage] = useContext(MessagesContext)
-const details = messages[`${folder}`].filter(item => item.id == id)[0]
-const {title, body, sender, receiver} = details
+  const [messages, setMessage] = useContext(MessagesContext);
+  const details = messages[`${folder}`].filter((item) => item.id == id)[0];
+  const { title, body, sender, receiver } = details;
 
-const deleteMessage = () => {
-    console.log(messages)
+  const deleteMessage = () => {
+    console.log(messages);
     setMessage({
-        ...messages,
-        [folder]:messages[`${folder}`].filter(item=> item !== details)})
+      ...messages,
+      [folder]: messages[`${folder}`].filter((item) => item !== details),
+    });
 
-    push(`/dash/${folder}`)
-}
+    push(`/dash/${folder}`);
+  };
 
-//const [details,setDetails] = useState({})
+  return (
+    <div>
+      <div>
+        <Icon alt="go back" onClick={goBack} />
+        <Icon img={reply} alt="reply" onClick={() => push("/dash/compose")} />
+      </div>
 
-
-    return <div>
-
-        <div>
-            <Icon alt='go back' onClick={goBack}/>
-            <Icon img={reply} alt = 'reply' onClick={()=>push('/dash/compose')}/>
-        </div>
-
-        <div>
+      <div>
         <h2>{title}</h2>
-        <h4>{folder === 'sent' ? `to: ${receiver}` : `from: ${sender}`}</h4>
+        <h4>{folder === "sent" ? `to: ${receiver}` : `from: ${sender}`}</h4>
         <p>{body}</p>
-        </div>
+      </div>
 
-        <div onClick={deleteMessage}>
-            <Icon 
-             img={deleteIcon}
-             alt='delete'/>
-        </div>
-
+      <div onClick={deleteMessage}>
+        <Icon img={deleteIcon} alt="delete" />
+      </div>
     </div>
-}
+  );
+};
 
-export default MessageDetails
+export default MessageDetails;
